8c658621e2ae048960b43e34c2d27f7af9323e2b
Davi <batatovisk0330@gmail.com>
2026-02-02 23:03:15 -0300

    testando o team no main

--- src/app/Main.java
+++ src/app/Main.java
@@ -7,13 +7,15 @@ public class Main {
     public static void main(String[] args) {
         Player p1 = new Player("Neymar",97);
         Player p2 = new Player("Messi",99);
+        Player p3 = new Player("Suarez",96);
 
         Team time = new Team("Barcelona");
         time.adicionarJogador(p1);
         time.adicionarJogador(p2);
+        time.adicionarJogador(p3);
         System.out.println(time);
-        for (Player p: time.getJogadores()) {
-            System.out.println(p);
-        }
+        System.out.println("Força do time: " + time.calcularForca());
+        System.out.println("Jogadores:");
+        time.getJogadores().forEach(System.out::println);
     }
 }
